0f6136375d9b050f4b8dce4a044bf53955106504
crusekev <crusekev@gmail.com>
2018-03-15 08:46:09 -0500

    daily grade for day 19

---
+++ src/kevincruse/Main.java
@@ -0,0 +1,43 @@
+package kevincruse;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        //incriment r by 5 on hw
+        int[][] myarray = new int[51][51]; //51 coloumns and 51 rows
+        //hw for(int r = 5;r <= 50;r+=5)
+        //for(c = 5;c <= 50;c+=5)
+        for (int r = 1; r <= 12; r++) {
+            System.out.printf("%5d", r);
+            for (int c = 1; c <= 12; c++) {
+                System.out.printf("%5d", r * c);
+                myarray[r][c] = r * c;
+            }
+            System.out.println();
+        }
+        FileWriter fWriter = new FileWriter("mydata.html");
+        PrintWriter outfile = new PrintWriter(fWriter);
+        outfile.println("<!DOCTYPE html>");
+        outfile.println("<html>");
+        outfile.println("<head>");
+        outfile.println("<title>Page Kevin Cruse </title>");
+        outfile.println("</head>");
+        outfile.println("<body>");
+        outfile.println("<h1> <center> Multiplication Table </center> </h1>");
+        outfile.println("<table style='border: 1px solid black;width:50%;text-align:center;'>");
+        for (int r = 1; r <= 12; r++) {
+            outfile.println("<tr>");
+            //outfile.println("<td>" +  + "</td>");
+            for (int c = 1; c <= 12; c++) {
+                outfile.println("<td>" + myarray[r][c] + "</td>");
+            }
+            outfile.println("</tr>");
+        }
+        outfile.println("</table>");
+        outfile.println("</body>");
+        outfile.println("</html>");
+        outfile.close();
+    }
+}
